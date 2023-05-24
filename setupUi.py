68b664f28ab6287f11955fc39cfac8201c920705
Connor Maaß <connor.maass@radtke-solutions.de>
2023-05-24 21:50:11 +0200

    ..

--- setupUi.py
+++ setupUi.py
@@ -1,7 +1,9 @@
 import os
+import threading
 import tkinter
 from tkinter import *
 from tkinter import ttk, font
+from tkinter.ttk import Style
 
 import _tkinter
 from pyrr.rectangle import height
@@ -16,8 +18,10 @@ class LineNumberText_NumberBouard(Canvas):
     def __init__(self, *args, **kwargs):
         Canvas.__init__(self, *args, **kwargs)
         self.textComp = None
+
     def attach(self, text_widget):
         self.textComp = text_widget
+
     def redraw(self, *args):
         self.delete("all")
         i = self.textComp.index("@0,0")
@@ -30,89 +34,123 @@ class LineNumberText_NumberBouard(Canvas):
             self.create_text(2, y, anchor="nw", text=linenum, fill="white",
                              font=font.Font(family="Bahnschrift", size=12, weight="bold"))
             i = self.textComp.index("%s+1line" % i)
+
+
 class LineNumberText_Text(Text):
     def __init__(self, *args, **kwargs):
         Text.__init__(self, *args, **kwargs)
-
+        self.configure(undo=True)
+        self.filetree=None
+        self._orig = self._w + "_orig"
         self._orig = self._w + "_orig"
         self.tk.call("rename", self._w, self._orig)
         self.tk.createcommand(self._w, self._proxy)
-        self.autocompletewindow=None
+        self.autocompletewindow = None
+        self.kwws = []
         self.genMarkers()
-        self.kwws=["func","controller","functio"]
-        self.autocompVisible=True
-        self.listbox:Listbox = None
-        self.autocompleteOptions=[]
-        self.autocompleteVar=Variable(value=self.autocompleteOptions)
+        self.openFileName=None
+
+        self.autocompVisible = True
+        self.listbox: Listbox = None
+        self.autocompleteOptions = []
+        self.autocompleteVar = Variable(value=self.autocompleteOptions)
 
         self.bind("<Key>", lambda u: self.after(10, self.queryKww))
 
+    def readJson(self):
+        with open("editor_kww.json", encoding="UTF-8") as f:
+            return json.loads(f.read())
+
     def genMarkers(self):
-        self.colection = [{"kww": ["if", "else","shr ",""],"kww2": ["include", "native",],"kww3": ["(", ")",";","[","]",],"kww4": ["func",]}]
-        self.tag_configure("kww", foreground=Collor.Warn)
+        self.colection = self.readJson()
+        for k in self.colection[0]:
+            self.kwws += self.colection[0][k]
         self.tag_configure("kww2", foreground="#B3679B")
         # strings #6A994E
         self.tag_configure("string", foreground="#6A994E")
+        self.tag_configure("<>", foreground="#32A287")
         self.tag_configure("kww3", foreground="#FBB02D")
         self.tag_configure("kww4", foreground="#0E79B2")
+        self.tag_configure("kww5", foreground="#F2C57C")
+        self.tag_configure("comment", foreground="#746F72")
 
-    def querylineForSpace(self,ind):
+    def querylineForSpace(self, ind):
         ind1 = ind.split(".")[0]
-        print("lst",self.index("end-1c linestart"))
-        line=self.get(ind1+".0",ind1+".end")
-        print("line",line)
-        for n,l in enumerate(line[::-1]):
-            if l==" ":
+        print("lst", self.index("end-1c linestart"))
+        line = self.get(ind1 + ".0", ind1 + ".end")
+        print("line", line)
+        for n, l in enumerate(line[::-1]):
+            if l == " ":
                 print("found Space")
-                return ind1+f".{len(line)-n}"
+                return ind1 + f".{len(line) - n}"
 
-        return ind1+".0"
-
+        return ind1 + ".0"
 
     def bindAutoComplete(self):
         def selectUper(e):
             if self.autocompVisible:
-                cur=self.listbox.curselection()
-                if cur ==():
-                    cur=(0,)
-                print("fdsfds",cur)
-                if cur[0]==0:
+                cur = self.listbox.curselection()
+                if cur == ():
+                    cur = (0,)
+                print("fdsfds", cur)
+                if cur[0] == 0:
                     return
                 self.listbox.select_clear(cur[0])
-                self.listbox.select_set(cur[0]-1)
-
+                self.listbox.select_set(cur[0] - 1)
 
                 return "break"
+
         def selectLower(e):
             print("kfjedwjo0ijoifgrejoijioüfdejiojhoiüferww")
             cur = self.listbox.curselection()
             if self.autocompVisible:
-                if cur ==():
-                    cur=(0,)
+                if cur == ():
+                    cur = (0,)
                 print("fdsfds", cur)
-                if cur[0]>=len(self.autocompleteOptions)-1:
+                if cur[0] >= len(self.autocompleteOptions) - 1:
                     return
                 self.listbox.select_clear(cur[0])
-                self.listbox.select_set(cur[0]+1)
+                self.listbox.select_set(cur[0] + 1)
                 return "break"
+
         def complete(e):
             cur = self.listbox.curselection()
             if self.autocompVisible:
                 if cur == ():
                     cur = (0,)
-                ins=self.index(INSERT)
-                spi=self.querylineForSpace(ins)
-                print("spi",spi)
-                self.replace(spi,ins,self.autocompleteOptions[cur[0]])
+                ins = self.index(INSERT)
+                spi = self.querylineForSpace(ins)
+                print("spi", spi)
+                self.replace(spi, ins, self.autocompleteOptions[cur[0]])
                 self.hidePopup()
-
 
                 return "break"
 
-        self.bind("<Down>",selectLower)#
-        self.bind("<Escape>",lambda u:self.hidePopup())
-        self.bind("<Tab>",complete)
-        self.bind("<Up>",selectUper)
+        self.bind("<Down>", selectLower)  #
+        self.bind("<Escape>", lambda u: self.hidePopup())
+        self.bind("<Tab>", complete)
+        self.master.bind("<Configure>", lambda u: self.hidePopup())
+        self.bind("<MouseWheel>", lambda u: self.hidePopup())
+        self.bind("<FocusOut>", complete)
+        self.bind("<Up>", selectUper)
+        self.bind("<Control-s>", self.save)
+    def save(self,u=None):
+        fn=self.openFileName
+        if fn==None:
+            return
+        with open(fn, "rb") as f:
+            emstack=f.read()
+
+        try:
+            with open(fn, "w") as f:
+
+                f.write( self.get("0.0",tkinter.END))
+        except Exception:
+            with open(fn, "wb") as f:
+                f.write(emstack)
+
+
+
     def _proxy(self, *args):
         try:
             cmd = (self._orig,) + args
@@ -129,42 +167,45 @@ class LineNumberText_Text(Text):
             self.event_generate("<<Change>>", when="tail")
 
         return result
+
     def hidePopup(self):
         self.autocompVisible = False
         self.autocompletewindow.withdraw()
-    def spawnPopup(self,index):
-        b=self.bbox(index)
+
+    def spawnPopup(self, index):
+        b = self.bbox(index)
         # print(b,index)
-        if b==None:
+        if b == None:
             return
-        x,y=b[:2]
-        x,y=x+self.winfo_rootx()-40,y+self.winfo_rooty()+20
-        #print("e",b)
+        x, y = b[:2]
+        x, y = x + self.winfo_rootx() - 40, y + self.winfo_rooty() + 20
+        # print("e",b)
 
         if not self.autocompletewindow:
-            self.autocompletewindow=w=Tk()
+            self.autocompletewindow = w = Tk()
             w.configure(bg=Collor.bg_selected)
             listbox = Listbox(
-                w,font=font.Font(family="Calibri",size=11,underline=False),borderwidth=0,highlightthickness=0,bg=Collor.bg_selected,fg=Collor.fg,
+                w, font=font.Font(family="Calibri", size=11, underline=False), borderwidth=0, highlightthickness=0,
+                bg=Collor.bg_selected, fg=Collor.fg,
                 selectbackground=Collor.selector_is,
 
                 height=6,
                 selectmode=tkinter.EXTENDED
             )
-            listbox.pack(pady=(3,3),padx=(5,5))
-            self.listbox=listbox
+            listbox.pack(pady=(3, 3), padx=(5, 5))
+            self.listbox = listbox
             w.overrideredirect(True)
             self.autocompVisible = True
             self.bindAutoComplete()
-            w.geometry("%s+%s"%(x,y))
+            w.geometry("+%s+%s" % (x, y))
         else:
             self.autocompletewindow.lift()
-            self.listbox.delete(0,tkinter.END)
-            for n,item in enumerate(self.autocompleteOptions):
-                self.listbox.insert(n,item)
+            self.listbox.delete(0, tkinter.END)
+            for n, item in enumerate(self.autocompleteOptions):
+                self.listbox.insert(n, item)
             self.listbox.update()
-            if len(self.autocompleteOptions)<=0:
-                self.autocompVisible=False
+            if len(self.autocompleteOptions) <= 0:
+                self.autocompVisible = False
                 self.autocompletewindow.withdraw()
             else:
                 self.autocompVisible = True
@@ -173,13 +214,13 @@ class LineNumberText_Text(Text):
             self.autocompletewindow.geometry("+%s+%s" % (x, y))
 
     def getPosible(self):
-        ins=self.index(tkinter.INSERT)
+        ins = self.index(tkinter.INSERT)
         self.autocompleteOptions.clear()
-        i2=self.querylineForSpace(ins)
-        value=self.get(i2,ins)
-        if value.replace(" ","")=="":
+        i2 = self.querylineForSpace(ins)
+        value = self.get(i2, ins)
+        if value.replace(" ", "") == "":
             return
-        print("Value",value,ins,i2)
+        print("Value", value, ins, i2)
 
         for e in self.kwws:
             if e.startswith(value):
@@ -187,9 +228,10 @@ class LineNumberText_Text(Text):
             else:
                 print(self.kwws)
 
-
         print(self.autocompleteOptions)
-
+        if self.listbox:
+            pass
+            self.listbox.configure(height=len(self.autocompleteOptions))
 
     def queryKww(self):
         self.getPosible()
@@ -198,30 +240,40 @@ class LineNumberText_Text(Text):
         self.spawnPopup(self.index(tkinter.INSERT))
 
         for tag in self.colection[0].keys():
-            self.tag_remove(tag,"0.0",self.index(tkinter.END))
+            self.tag_remove(tag, "0.0", self.index(tkinter.END))
         self.tag_remove("string", "0.0", self.index(tkinter.END))
+
+        self.tag_remove("<>", "0.0", self.index(tkinter.END))
 
         for tag in self.colection[0].keys():
             for a in self.colection[0][tag]:
-                st="0.0"
-                end="0.0"
-                stOld=""
-                while self.index(st)!=self.index(tkinter.END):
+                st = "0.0"
+                end = "0.0"
+                stOld = ""
+                while self.index(st) != self.index(tkinter.END):
                     st = self.search(a, end, tkinter.END)
-                    if stOld==st:
-
-
+                    if stOld == st:
                         break
                     stOld = st
 
                     print(st)
                     end = self.index('%s+%dc' % (st, len(a)))
-
-                    self.tag_add(tag, st, end)
+                    alowedpars = ["[", "]" "(", ")", ".", " ", "\n"]
+                    print("----", self.get('%s+%dc' % (end, 1)))
+                    ec = self.get(self.index(end), self.index('%s+%dc' % (end, 1)))
+                    bc = self.get(self.index('%s-%dc' % (st, 1)), self.index(st))
+                    if (ec == "None") | (ec in alowedpars):
+                        if (bc == "None") | (bc in alowedpars):
+                            self.tag_add(tag, st, end)
+                        else:
+                            print("bc", bc, a, type(ec))
+                    else:
+                        print("ec", ec, a, type(ec))
 
         st = "0.0"
         end = "0.0"
-        L=[]
+        L = []
+        L2 = []
         stOld = ""
         while self.index(st) != self.index(tkinter.END):
             st = self.search("\"", end, tkinter.END)
@@ -229,27 +281,58 @@ class LineNumberText_Text(Text):
                 break
             stOld = st
 
-            #print(st)
+            # print(st)
             end = self.index('%s+%dc' % (st, 1))
-            L+=[st]
+            L += [st]
 
+        st = "0.0"
+        end = "0.0"
 
+        stOld = ""
+        while self.index(st) != self.index(tkinter.END):
+            st = self.search("<", end, tkinter.END)
+            if stOld == st:
+                break
+            stOld = st
 
-        while len(L)>0:
+            # print(st)
+            end = self.search(">", st, tkinter.END)
+            L2 += [st, end]
 
-            b=L.pop(0)
-            try:
-                e=L.pop(0)
-            except IndexError:
+        L3 = []
+        st = "0.0"
+        end = "0.0"
+        while self.index(st) != self.index(tkinter.END):
+            st = self.search("//", end, tkinter.END)
+            if stOld == st:
+                break
+            stOld = st
 
-                e=self.index(tkinter.END)
-                #print(e)
+            # print(st)
+            end = self.search("\n", st, tkinter.END)
+            L3 += [st, end]
 
-            for tag in self.colection[0].keys():
-                self.tag_remove(tag,b,self.index('%s+%dc' % (e,1)))
-            self.tag_add("string",b,self.index('%s+%dc' % (e,1)))
+        def placeMarkersFromList(list, tagname):
 
-        #print("getString")
+            while len(list) > 0:
+
+                b = list.pop(0)
+                try:
+                    e = list.pop(0)
+                except IndexError:
+
+                    e = self.index(tkinter.END)
+                    # print(e)
+
+                for tag in self.colection[0].keys():
+                    self.tag_remove(tag, b, self.index('%s+%dc' % (e, 1)))
+                self.tag_add(tagname, b, self.index('%s+%dc' % (e, 1)))
+
+        placeMarkersFromList(L2, "<>")
+        placeMarkersFromList(L3, "comment")
+        placeMarkersFromList(L, "string")
+
+        # print("getString")
 
     def getStringSeperated(string, splitonspace=True):
         retStr = [""]
@@ -282,29 +365,76 @@ class LineNumberText_Text(Text):
         return retStr
 
 
-
-def openEditor():
+def openEditor(mainfile,dict1):
     t = Tk()
+    t.title("Editor")
+    t.configure(bg=Collor.bg_lighter)
     a = FileTree(height=30, width=400)
     f = Frame(bg=Collor.bg_lighter)
-    f.pack()
-    a.generateFileTree(r"C:\Users\ctind\PycharmProjects\PYDatabase\minedashbin\\minedashbin")
+    fb=Frame(bg=Collor.bg_lighter)
+    s=Style()
+    s.theme_use("clam")
+    s.configure("special1.Horizontal.TProgressbar", foreground=Collor.Neutral, background=Collor.fg,troughcolor=Collor.bg,darkcolor=Collor.bg_lighter,lightcolor=Collor.bg_lighter,bordercolor=Collor.bg_lighter)
+    p=ttk.Progressbar(
+    fb,
+    orient='horizontal',
+    mode='determinate',
+    length=280,style="special1.Horizontal.TProgressbar"
+    )
+    f.pack(fill="x")
+    fb.pack(fill="x",side="bottom")
+    lastbuildInfo=createLabel1(fb,"",bg=Collor.bg_lighter)
+    lastbuildInfo.pack(side="right",anchor="se",padx=(0,20))
 
-    def openfile(e):
-        file = a.getFile()
-        with open(file,encoding='utf-8') as f:
 
+
+
+    b=LabelButton(master=f,img=createImage("imgs/compile_64.png",20,20))
+    def compile():
+        p.pack(side="right",padx=(0,20))
+        p.start(8)
+        p.update()
+        ret=dict1["compile"]()
+
+        p.stop()
+        p.pack_forget()
+        if ret==False:
+            lastbuildInfo.configure(fg=Collor.Error,text=" Build failed")
+            return
+
+        lastbuildInfo.configure(fg=Collor.Success,text="Build finished after "+ret)
+        lastbuildInfo.after(8000,lambda :lastbuildInfo.configure(text=""))
+    def compThread():
+        threading.Thread(target=compile).start()
+
+    b.command(compThread)
+    b.pack(side="right",anchor="e")
+
+
+    print("path",mainfile[::-1].split("/",1)[1][::-1])
+    a.generateFileTree(mainfile[::-1].split("/",1)[1][::-1].replace("/","\\"))
+
+    def openfile(e=None,file=None):
+        text.save()
+        if not file:
+            file = a.getFile()
+        with open(file=file,mode="r", encoding='utf-8') as f:
             text.replace("0.0", tkinter.END, "")
             text.insert("0.0", f.read())
             text.queryKww()
+        text.openFileName=file
 
     a.bind("<<OpenFile>>", openfile)
+
 
     a.pack(side="left")
     tel = LineNumberText_NumberBouard(t, bg=Collor.bg_lighter, width=30,
                                       highlightbackground=Collor.bg)
     text = LineNumberText_Text(t, insertbackground=Collor.fg, font=font.Font(size=13, family="Calibre"), bg=Collor.bg,
                                fg=Collor.fg)
+    text.filetree=a
+    openfile("",mainfile)
+
     text.pack(side="right", fill="y", expand=True)
     tel.pack(side="right", fill="y", expand=True)
     text.bind("<<Change>>", tel.redraw)
@@ -314,8 +444,7 @@ def openEditor():
     t.mainloop()
 
 
-openEditor()
-exit()
+
 
 
 def getPathFile(i, ft, fileOfset=""):
@@ -335,7 +464,7 @@ def getPathDir(i, stateafter="normal"):
         i.setValue(path)
 
 
-def open():
+def openSetup():
     tk = Window()
     tk.wm_geometry("400x340")
     tk.resizable(True, False)
@@ -359,7 +488,7 @@ def open():
     createLabel1(frame, "", font=f).pack()
     createLabel1(frame, "", font=f).pack()
 
-    i = TextBox(master=frame, height=1, width=20, font=f2, insertbackground=Collor.fg)
+    i = TextBox(master=frame, height=1, width=20, font=f2)
     i.pack(anchor="center")
 
     # page 2
@@ -372,7 +501,7 @@ def open():
     createLabel1(frame, "", font=f).pack()
     createLabel1(frame, "", font=f).pack()
 
-    i2 = TextBox(master=frame, height=1, width=20, font=f2, insertbackground=Collor.fg, state="disabled")
+    i2 = TextBox(master=frame, height=1, width=20, font=f2, state="disabled")
     i2.pack(anchor="center")
     B = BetterButton(master=frame, text="Select Source", borderwidth=1,
                      command=lambda: getPathFile(i2, (("mcdb", "*.mcdb"), ("all", "*.*"))))
@@ -388,7 +517,7 @@ def open():
     createLabel1(frame, "", font=f).pack()
     createLabel1(frame, "", font=f).pack()
 
-    i3 = TextBox(master=frame, height=1, width=20, font=f2, insertbackground=Collor.fg, state="disabled")
+    i3 = TextBox(master=frame, height=1, width=20, font=f2, state="disabled")
     i3.pack(anchor="center")
     B = BetterButton(master=frame, text="Select Output", borderwidth=1, command=lambda: getPathDir(i3, ))
     B.pack()
@@ -411,7 +540,7 @@ def open():
             i.configure(bg=Collor.Error)
 
             return
-        name, file, out = i.getValue().lower(), i2.getValue().lower(), i3.getValue().lower()
+        name, file, out = i.getValue().lower(), i2.getValue(), i3.getValue()
         if not (os.path.exists(file)):
             N.nav(1)
             i2.configure(bg=Collor.Error)
